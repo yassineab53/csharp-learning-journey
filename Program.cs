7717daee3a7fb02e3d3599fc0134616480b66a28
yassine4real <yssnabbou2002@gmail.com>
2024-11-19 12:40:43 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double value = 1000D / 12.53D;
+
+            Console.WriteLine(value);
+            Console.WriteLine(string.Format("{0:0.00}", value));
+
+            Console.ReadLine();
+        }
+    }
+}
