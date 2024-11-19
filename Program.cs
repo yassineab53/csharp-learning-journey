f3373de1ce73c17a1b76fd87e491c0a4028d1c09
yassine4real <yssnabbou2002@gmail.com>
2024-11-19 15:51:19 +0100

    TryParse

--- Program.cs
+++ Program.cs
@@ -11,10 +11,20 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            double value = 1000D / 12.53D;
+            // Numeric formatting
+            /*double value = 1000D / 12.53D;
 
             Console.WriteLine(value);
-            Console.WriteLine(string.Format("{0:0.00}", value));
+            Console.WriteLine(string.Format("{0:0.00}", value)); */
+
+            //TryParse 
+            Console.WriteLine("Enter a number : ");
+            string numInput = Console.ReadLine();
+            //int n = Convert.ToInt32(st);
+            int num = 0;
+
+            int.TryParse(numInput, out num);
+            Console.WriteLine(num);
 
             Console.ReadLine();
         }
